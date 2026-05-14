85575bd4ff25468f5e0fc88270dc77db9d5af3a4
Anna <annasworkandstudy2025@gmail.com>
2026-05-15 01:49:03 +0500

    Составлены тесты для API сервиса

---
+++ url.py
@@ -0,0 +1,8 @@
+
+class URL:
+
+    BASE_URL='https://qa-scooter.praktikum-services.ru'
+    CREATE_COURIER=f"{BASE_URL}/api/v1/courier"
+    LOGIN_COURIER=f"{BASE_URL}/api/v1/courier/login"
+    CREATE_ORDER=f"{BASE_URL}/api/v1/orders"
+    LIST_ORDER=f"{BASE_URL}/api/v1/orders"
